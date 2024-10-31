f6272a7d666343509c3188a8408fa82fde4419ed
QuangThinhTran <tranlequangthinh24122002@email.com>
2024-10-31 19:38:26 +0700

    fix: Config depencencies

--- src/components/template/Home/index.tsx
+++ src/components/template/Home/index.tsx
@@ -6,10 +6,10 @@ import useFetch from '../../../common/hooks/useFetch';
 import { BLOG } from '../../../common/enums/route';
 
 const Home = () => {
-  const { data, error } = useFetch(BLOG.GET);
-  if (error) {
-    return <p>Error: {error}</p>;
-  }
+  // const { data, error } = useFetch(BLOG.GET);
+  // if (error) {
+  //   return <p>Error: {error}</p>;
+  // }
 
   return (
     <Container>
@@ -17,11 +17,11 @@ const Home = () => {
         <h4>Share yours ideas</h4>
         <Form rows={3} />
       </div>
-      {
+      {/* {
         data?.map((blog: IBlog) => (
           <Blog {...blog} key={blog.id} />
         ))
-      }
+      } */}
     </Container>
   )
 }
